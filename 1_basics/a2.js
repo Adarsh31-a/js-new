528ede86950e2584fdc321275fd73611b2b06707
Adarsh31-a <adarshk8188@gmail.com>
2026-05-25 18:06:50 +0000

    datatypes

---
+++ 1_basics/a2.js
@@ -0,0 +1,14 @@
+const accountId = 3015
+let email = "adarsk@gmail.com"
+var accountpass="1234"
+accountCity="Jaipur"
+
+
+// accoubtId= 3 not allowed to change
+email="kartik@gmail.com"
+accountpass="0832"
+accountCity="Etawah"
+let accountState
+
+console.log(accountId);
+console.table([email,accountpass,accountCity,accountState])
