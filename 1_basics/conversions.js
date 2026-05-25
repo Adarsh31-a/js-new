0741886861ceed89d37687fe2a47d616fa63447c
Adarsh31-a <adarshk8188@gmail.com>
2026-05-25 18:32:04 +0000

    conversion of datatypes

---
+++ 1_basics/conversions.js
@@ -0,0 +1,19 @@
+let score="33"
+
+console.log(typeof score);
+
+let num = Number(score)
+console.log(typeof num);
+
+// 33 => number 
+// "33shj" => NaN
+// true => 1; false => 0;
+
+
+//Boolean
+let bool=""
+let changedto= Boolean(bool)
+console.log(changedto);
+
+// 1 => true; 0=>false
+// "" => false ; "igkdb" => true
