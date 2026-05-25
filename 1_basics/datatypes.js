17c9105735d605a1d5681ef5c1f0009cd6efef97
Adarsh31-a <adarshk8188@gmail.com>
2026-05-25 18:06:27 +0000

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as newer version
+
+//alert(3+3) // this can be shown on nrowsers not on node js right now
+
+console.log(3+3)
+console.log("Hitesh");
+
+let name = "Adarsh"
+let age = 19
+let isyes = true
+
+//string=""
+// number >=2 && less than power 53
+// boolean
+// null // standalone value
+// undefined => not assigned
+// symbol => uniqueness
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
