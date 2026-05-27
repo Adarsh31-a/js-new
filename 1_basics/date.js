81410e20effe03fd4562016c2b507da0d8666da3
Adarsh31-a <adarshk8188@gmail.com>
2026-05-27 13:14:23 +0000

    completing of basics

---
+++ 1_basics/date.js
@@ -0,0 +1,26 @@
+const mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString()); //only date 
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString()); // date like 1/2/2026
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleString());
+
+console.log(typeof mydate);
+
+// const mycreateddate = new Date(2026, 0, 23)
+const mycreateddate = new Date(2026, 0, 23, 18, 34)
+// console.log(mycreateddate.toLocaleString());
+
+let mytimestamp = Date.now()
+// console.log(mytimestamp);
+// console.log(mycreateddate.getTime());
+
+// to convert in seconds 
+
+// console.log(Math.round(mycreateddate.getTime()/1000));
+
+const mynewdate = new Date()
+console.log(mynewdate);
+
+
---
+++ 1_basics/number.js
@@ -0,0 +1,30 @@
+
+// const num = new Number(100)
+// console.log(num);
+
+// console.log(num.toString().length)
+// console.log(num.toFixed(2));
+
+// const num2= 135.69
+
+// console.log(num2.toPrecision(4));
+
+// const n=1000000
+// console.log(n.toLocaleString('en-IN'));
+// console.log(Number.MAX_ALUE);
+
+// ++++++++++++++++++++Maths++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-7));
+// console.log(Math.sqrt(24).toPrecision(5));
+// console.log(Math.min(1,3,9,2));
+// console.log(Math.max(1,3,9,2));
+// console.log(Math.round(4.3));
+console.log(Math.random())
+console.log(Math.round(Math.random()*10)+1) // +1 to avoid answer as 0
+
+const min=20
+const max=40
+
+console.log(Math.round(Math.random()*(max-min+1)) +min);
