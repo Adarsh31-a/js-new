5151617da66350789403130d86e21a4232976fff
Adarsh31-a <adarshk8188@gmail.com>
2026-05-30 14:29:25 +0000

    arrow

--- 3_basics/03_arrow.js
+++ 3_basics/03_arrow.js
@@ -1,4 +1,34 @@
+// const { use } = require("react");
+
 const user = {
     username : "Adarsh",
-    price : 990
+    price : 990,
+  welcomemessage : function(){
+    console.log(`${this.username} ,welcome to this website`);
+    console.log(this);
+    
+  }
+
 }
+// (this) will give you the current context 
+// Iside browser the global object is window
+//(this) cannot be used inside a function
+
+// user.welcomemessage()
+// user.username = "Kartik"
+// user.welcomemessage()
+
+function love(){
+    const name = "Akdom"
+    console.log(this.name);
+    
+}
+// love()
+
+//ARROW FUNCTION syntax
+ ()=>{}
+
+// const addtwonum = (num1,num2)=> num1*num2
+
+const addtwonum = (num1,num2)=>({name : "kartik"}) // to deal with objects 
+console.log(addtwonum(9,29));
